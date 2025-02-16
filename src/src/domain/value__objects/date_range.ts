16fcdd0ba769c024924f89164d9fb10b11e3f99a
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-02-16 12:44:09 -0300

    refactor: new method to validate and thorw error if we pass a invalid date

--- src/src/domain/value__objects/date_range.ts
+++ src/src/domain/value__objects/date_range.ts
@@ -3,11 +3,18 @@ export class DateRange {
   private readonly endDate: Date;
 
   constructor(startDate: Date, endDate: Date) {
-    if (endDate <= startDate) {
-      throw new Error('A data de término deve ser posterior à data de início.');
-    }
+    this.validateDates(startDate, endDate);
     this.startDate = startDate;
     this.endDate = endDate;
+  }
+
+  private validateDates(startDate: Date, endDate: Date): void {
+    if (startDate == endDate) {
+      throw new Error('A data de início e término não pode ser iguais');
+    }
+    if (endDate < startDate) {
+      throw new Error('A data de término deve ser posterior à data de início.');
+    }
   }
 
   getStartDate(): Date {
